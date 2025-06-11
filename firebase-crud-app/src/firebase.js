f23ecd140793a7c862f265ed1d4fd672c1e1a17b
Deepak-vm-2024 <Deepakvm2024@gmail.com>
2025-06-11 09:13:10 -0700

    try

--- firebase-crud-app/src/firebase.js
+++ firebase-crud-app/src/firebase.js
@@ -1,15 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAlBdl1Gl6Drr6x_lpLtEM5u0mowZ_PlyA",
+  authDomain: "deephub-49708.firebaseapp.com",
+  projectId: "deephub-49708",
+  storageBucket: "deephub-49708.firebasestorage.app",
+  messagingSenderId: "735289523656",
+  appId: "1:735289523656:web:c034a7378565b9beedd4fd",
+  measurementId: "G-RTNSYCH3S1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
